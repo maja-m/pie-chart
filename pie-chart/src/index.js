261aed05acfc3c7f2ff6444a76b564b4e224fd25
Maja Marchewka <marchewka.maja@gmail.com>
2018-05-12 15:21:26 +0200

    zadanie 2 i 3

--- pie-chart/src/index.js
+++ pie-chart/src/index.js
@@ -2,9 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+//Zadanie 2
+const zadanie2 = `
+Zadanie 2:
+const myList = [1, 2, 3];
+for (let i = 0; i < myList.length; i++) {
+  console.log(myList[i]);
+}`;
+
 ReactDOM.render(
-  <div id="pie-chart">
-    <App />
+  <div>
+    <div id="pie-chart">
+      <App />
+    </div>
+    <pre>{zadanie2}</pre>
+    <div>{odp}</div>
   </div>,
   document.getElementById("root")
 );
+
+//Zadanie 3
+function isAPalindrome(word) {
+  word = word
+    .toLowerCase()
+    .split(" ")
+    .join("");
+
+  let wordTemp = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    wordTemp += word[i];
+  }
+
+  return word == wordTemp;
+}
+
+console.log(isAPalindrome("Może jutro ta dama da tortu jeżom"));
+
+//Zadanie 4
+const odp = "";
